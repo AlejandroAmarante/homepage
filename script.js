cfea564df921e03f9d81d8047c6fd4bf4c1af450
AlejandroMontalvo <alejandrormont@gmail.com>
2024-09-11 23:00:30 -0400

    minor style refactoring

--- script.js
+++ script.js
@@ -1,81 +1,93 @@
 window.addEventListener("DOMContentLoaded", () => {
   console.log("Page is fully loaded");
-  const defaultSearchEngine = "google";
+
+  const defaultSearchEngine = "startpage";
   setSearchEngineLogo(defaultSearchEngine);
 
   const currentDateElement = document.getElementById("date");
+  const timeElement = document.getElementById("time");
   const today = new Date();
   currentDateElement.textContent = formatDate(today);
 
   // Optional: Update the time every second
-  setInterval(function () {
-    const timeElement = document.getElementById("time");
+  let lastTimeUpdate = 0;
+  function updateClock() {
     const now = new Date();
-    timeElement.textContent = formatTime(now);
-  }, 1000);
+    const currentTime = now.getSeconds();
+    if (currentTime !== lastTimeUpdate) {
+      lastTimeUpdate = currentTime;
+      timeElement.textContent = formatTime(now);
+    }
+    requestAnimationFrame(updateClock);
+  }
+  updateClock();
+
+  const apiKey = localStorage.getItem("apiKey");
+  if (apiKey) {
+    const apiKeyInput = document.getElementById("api-key");
+    apiKeyInput.value = apiKey;
+  }
 
   getLocationAndWeather();
 });
 
-// JavaScript to set the date
 function formatDate(date) {
-  const options = {
+  return date.toLocaleDateString("en-US", {
     weekday: "short",
     month: "short",
     day: "numeric",
     year: "numeric",
-  };
-  return date.toLocaleDateString("en-US", options);
+  });
 }
 
 function formatTime(date) {
-  const timeOptions = {
+  return date.toLocaleTimeString("en-US", {
     hour: "2-digit",
     minute: "2-digit",
     second: "2-digit",
     hour12: true,
-  };
-  return date.toLocaleTimeString("en-US", timeOptions);
+  });
 }
 
 // Function to get location and weather
 function getLocationAndWeather() {
   if (navigator.geolocation) {
+    if (localStorage.getItem("apiKey")) {
+      document.getElementById("weather").textContent = "Loading...";
+    } else {
+      document.getElementById("weather").textContent = "Inactive";
+    }
     navigator.geolocation.getCurrentPosition(
-      (position) => {
-        console.log("Geolocation success"); // Confirm the geolocation is working
-        const lat = position.coords.latitude;
-        const lon = position.coords.longitude;
+      ({ coords }) => {
+        const lat = coords.latitude;
+        const lon = coords.longitude;
 
-        // Call OpenWeather API to get weather data
-        const apiKey = localStorage.getItem("apiKey"); // Replace with your OpenWeather API key
+        const apiKey = localStorage.getItem("apiKey");
         const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
 
         fetch(apiUrl)
           .then((response) => response.json())
           .then((data) => {
-            // Display location and weather information
             const locationElement = document.getElementById("location");
             const weatherElement = document.getElementById("weather");
+            const {
+              name: city,
+              main: { temp },
+              weather,
+            } = data;
 
-            const city = data.name;
-            const temp = data.main.temp;
-            const description = data.weather[0].description;
-
-            locationElement.textContent = `${city}`;
-            weatherElement.textContent = `${temp}°C, ${description}`;
+            locationElement.textContent = city;
+            weatherElement.textContent = `${temp}°C, ${weather[0].description}`;
           })
           .catch((error) =>
             console.error("Error fetching weather data:", error)
           );
       },
       (error) => {
-        // Log error messages for geolocation issues
         console.error("Geolocation error:", error.message);
-        document.getElementById("location").textContent =
-          "Location not available";
+        document.getElementById("location").textContent = "OpenWeather API";
         document.getElementById("weather").textContent =
-          "Weather not available";
+          "Error";
       }
     );
   } else {
@@ -85,11 +97,11 @@ function getLocationAndWeather() {
 
 const searchInput = document.getElementById("searchInput");
 const logo = document.getElementById("logo");
-let currentSearchEngine = "google"; // Track the current search engine
+let currentSearchEngine = "startpage";
 
 function setSearchEngineLogo(searchEngine) {
   if (searchEngine !== currentSearchEngine && logo.src != null) {
-    const logoSrc = getLogoSrc(searchEngine);
+    const logoSrc = `./imgs/${searchEngine}-icon.svg`;
 
     // Reduce the opacity of the logo to 0
     logo.style.opacity = "0";
@@ -111,105 +123,65 @@ function setSearchEngineLogo(searchEngine) {
       currentSearchEngine = searchEngine; // Update the current search engine
     });
   } else {
-    const logoSrc = getLogoSrc(searchEngine);
+    const logoSrc = `./imgs/${searchEngine}-icon.svg`;
     logo.src = logoSrc;
   }
 }
 
-function getLogoSrc(searchEngine) {
-  switch (searchEngine) {
-    case "duckduckgo":
-      return "./imgs/duckduckgo-icon.svg";
-    case "bing":
-      return "./imgs/bing-icon.svg";
-    case "google":
-    default:
-      return "./imgs/google-icon.svg";
-  }
-}
-
-searchInput.addEventListener("input", function () {
+searchInput.addEventListener("input", () => {
   const searchString = searchInput.value.trim();
-  let searchEngine = "google"; // Default search engine
-
-  if (searchString.startsWith(":ddg")) {
-    searchEngine = "duckduckgo";
-  } else if (searchString.startsWith(":b")) {
-    searchEngine = "bing";
-  } else if (searchString.startsWith(":g")) {
-    searchEngine = "google";
-  }
+  const searchEngine = searchString.startsWith(":ddg")
+    ? "duckduckgo"
+    : searchString.startsWith(":b")
+    ? "bing"
+    : searchString.startsWith(":g")
+    ? "google"
+    : "startpage";
 
   setSearchEngineLogo(searchEngine);
 });
 
-searchInput.addEventListener("keydown", function (event) {
+searchInput.addEventListener("keydown", (event) => {
   if (event.key === "Enter") {
     const searchString = searchInput.value.trim();
-    const searchEngineModifiers = [":ddg", ":b", ":g"];
-
-    if (
-      searchString.length > 0 &&
-      !searchEngineModifiers.includes(searchString)
-    ) {
-      let query = searchString;
-
-      if (searchString.startsWith(":ddg")) {
-        query = searchString.split(" ").slice(1).join(" ");
-        window.location.href = `https://duckduckgo.com/?q=${encodeURIComponent(
-          query
-        )}`;
-      } else if (searchString.startsWith(":b")) {
-        query = searchString.split(" ").slice(1).join(" ");
-        window.location.href = `https://www.bing.com/search?q=${encodeURIComponent(
-          query
-        )}`;
-      } else if (searchString.startsWith(":g")) {
-        query = searchString.split(" ").slice(1).join(" ");
-        window.location.href = `https://www.google.com/search?q=${encodeURIComponent(
-          query
-        )}`;
-      } else if (searchString.startsWith(":sp")) {
-        window.location.href = `https://www.startpage.com/do/search?query=${encodeURIComponent(
-          searchString
-        )}`;
-      } else {
-        window.location.href = `https://www.google.com/search?q=${encodeURIComponent(
-          searchString
-        )}`;
-      }
+    if (searchString.length > 0) {
+      let query = searchString.split(" ").slice(1).join(" ");
+      const searchUrl = searchString.startsWith(":ddg")
+        ? `https://duckduckgo.com/?q=${encodeURIComponent(query)}`
+        : searchString.startsWith(":b")
+        ? `https://www.bing.com/search?q=${encodeURIComponent(query)}`
+        : searchString.startsWith(":g")
+        ? `https://www.google.com/search?q=${encodeURIComponent(query)}`
+        : `https://www.startpage.com/do/search?query=${encodeURIComponent(
+            query
+          )}`;
+      window.location.href = searchUrl;
     }
   }
 });
 
-let locationWeatherElement = document.getElementById("location-weather");
-let modal = document.getElementById("modal");
-let modalContent = document.getElementById("modal-content");
-locationWeatherElement.addEventListener("click", function () {
-  // Show the modal
-  modal.style.display = "flex";
+document.getElementById("location-weather").addEventListener("click", () => {
+  const modal = document.getElementById("modal");
+  const modalTitle = document.getElementById("modal-title");
+  const modalText = document.getElementById("modal-text");
 
-  // Optionally populate modal content
+  modal.style.display = "flex";
   modalTitle.textContent = "Location and Weather Details";
   modalText.textContent =
     "Here's more detailed information about your location and weather.";
 });
 
-let apiKeyInput = document.getElementById("api-key");
-apiKeyInput.addEventListener("input", function () {
-  // Save the API key to local storage
+const apiKeyInput = document.getElementById("api-key");
+apiKeyInput.addEventListener("input", () => {
   localStorage.setItem("apiKey", apiKeyInput.value);
+  if (apiKeyInput.value) {
+    setTimeout(() => window.location.reload(), 1000);
+  }
 });
 
-// Get the API key from local storage
-let apiKey = localStorage.getItem("apiKey");
-if (apiKey) {
-  apiKeyInput.value = apiKey;
-}
-
-// Add event to close the modal when clicking outside the modal content
-window.addEventListener("click", function (event) {
+window.addEventListener("click", (event) => {
+  const modal = document.getElementById("modal");
   if (event.target == modal) {
-    modal.style.display = "none"; // Close the modal
+    modal.style.display = "none";
   }
 });

7f570a7c0a5810613a1962e299a64bf74578fd59
AlejandroMontalvo <alejandrormont@gmail.com>
2024-09-18 18:35:07 -0400

    searchinput refactoring #1

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,6 @@
 window.addEventListener("DOMContentLoaded", () => {
   console.log("Page is fully loaded");
 
-  const defaultSearchEngine = "startpage";
-  setSearchEngineLogo(defaultSearchEngine);
-
   const today = new Date();
   document.getElementById("date").textContent = formatDate(today);
 
--- scripts/searchInput.js
+++ scripts/searchInput.js
@@ -1,28 +1,40 @@
-let currentSearchEngine = "startpage";
-
-const searchEngines = [
+const defualtSearchEngines = [
   {
+    searchEngine: "startpage",
     alias: ":sp",
     searchLink: "https://startpage.com/do/search?query=",
     searchIcon: "./imgs/startpage-icon.svg",
     isDefualt: true,
   },
   {
+    searchEngine: "bing",
     alias: ":ddg",
     searchLink: "https://duckduckgo.com/?q=",
     searchIcon: "./imgs/duckduckgo-icon.svg",
+    isDefualt: false,
   },
   {
+    searchEngine: "bing",
     alias: ":b",
     searchLink: "https://bing.com/search?q=",
     searchIcon: "./imgs/bing-icon.svg",
+    isDefualt: false,
   },
   {
+    searchEngine: "google",
     alias: ":g",
     searchLink: "https://www.google.com/search?q=",
     searchIcon: "./imgs/google-icon.svg",
+    isDefualt: false,
   },
 ];
+
+console.log(localStorage.getItem("savedSearchEngines"));
+let currentSearchEngine = JSON.parse(
+  localStorage.getItem("savedSearchEngines")
+).find((engine) => engine.isDefault);
+
+console.log(currentSearchEngine);
 
 function setSearchEngineLogo(searchEngine) {
   const logo = document.getElementById("logo");
@@ -48,47 +60,58 @@ function initializeSearchEngineHandler() {
     "search-engines-textarea"
   );
 
-  if (localStorage.getItem("searchEngines")) {
-    searchEnginesTextarea.value = localStorage.getItem("searchEngines");
+  if (localStorage.getItem("savedSearchEngines")) {
+    searchEnginesTextarea.value = localStorage.getItem("savedSearchEngines");
   } else {
-    searchEnginesTextarea.value = JSON.stringify(searchEngines, null, 2);
+    searchEnginesTextarea.value = JSON.stringify(defualtSearchEngines, null, 2);
+    localStorage.setItem(
+      "savedSearchEngines",
+      JSON.stringify(defualtSearchEngines)
+    );
   }
 
-  searchInput.addEventListener("input", () => {
-    const searchString = searchInput.value.trim();
-    const searchEngine = searchString.startsWith(":ddg")
-      ? "duckduckgo"
-      : searchString.startsWith(":b")
-      ? "bing"
-      : searchString.startsWith(":g")
-      ? "google"
-      : "startpage";
-    setSearchEngineLogo(searchEngine);
+  searchEnginesTextarea.addEventListener("input", () => {
+    localStorage.setItem(
+      "savedSearchEngines",
+      JSON.stringify(searchEnginesTextarea.value)
+    );
   });
 
-  searchInput.addEventListener("keydown", (event) => {
-    if (event.key === "Enter") {
-      const searchString = searchInput.value.trim();
-      if (searchString.length > 0) {
-        let query = searchString;
-        if (
-          searchString.startsWith(":ddg") ||
-          searchString.startsWith(":b") ||
-          searchString.startsWith(":g")
-        ) {
-          query = searchString.split(" ").slice(1).join(" ");
-        }
-        const searchUrl = searchString.startsWith(":ddg")
-          ? `https://duckduckgo.com/?q=${encodeURIComponent(query)}`
-          : searchString.startsWith(":b")
-          ? `https://www.bing.com/search?q=${encodeURIComponent(query)}`
-          : searchString.startsWith(":g")
-          ? `https://www.google.com/search?q=${encodeURIComponent(query)}`
-          : `https://www.startpage.com/do/search?query=${encodeURIComponent(
-              searchString
-            )}`;
-        window.location.href = searchUrl;
-      }
-    }
-  });
+  // searchInput.addEventListener("input", () => {
+  //   const searchString = searchInput.value.trim();
+  //   const searchEngine = searchString.startsWith(":ddg")
+  //     ? "duckduckgo"
+  //     : searchString.startsWith(":b")
+  //     ? "bing"
+  //     : searchString.startsWith(":g")
+  //     ? "google"
+  //     : "startpage";
+  //   setSearchEngineLogo(searchEngine);
+  // });
+
+  // searchInput.addEventListener("keydown", (event) => {
+  //   if (event.key === "Enter") {
+  //     const searchString = searchInput.value.trim();
+  //     if (searchString.length > 0) {
+  //       let query = searchString;
+  //       if (
+  //         searchString.startsWith(":ddg") ||
+  //         searchString.startsWith(":b") ||
+  //         searchString.startsWith(":g")
+  //       ) {
+  //         query = searchString.split(" ").slice(1).join(" ");
+  //       }
+  //       const searchUrl = searchString.startsWith(":ddg")
+  //         ? `https://duckduckgo.com/?q=${encodeURIComponent(query)}`
+  //         : searchString.startsWith(":b")
+  //         ? `https://www.bing.com/search?q=${encodeURIComponent(query)}`
+  //         : searchString.startsWith(":g")
+  //         ? `https://www.google.com/search?q=${encodeURIComponent(query)}`
+  //         : `https://www.startpage.com/do/search?query=${encodeURIComponent(
+  //             searchString
+  //           )}`;
+  //       window.location.href = searchUrl;
+  //     }
+  //   }
+  // });
 }
